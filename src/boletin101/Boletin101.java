e94af1fd0ab121c01bd30ad5e48bd119f6d72162
sbenavidesvazquez <sbenavidesvazquez@eniac24.danielcastelao.org>
2015-01-19 08:57:43 +0100

    com 1

---
+++ src/boletin101/Boletin101.java
@@ -0,0 +1,17 @@
+
+package boletin101;
+
+
+public class Boletin101 {
+
+    
+    public static void main(String[] args) {
+        Juego obx=new Juego();
+        
+        obx.intentos();
+        obx.numero();
+        obx.adivinar();
+        obx.fin();
+    }
+    
+}
---
+++ src/boletin101/Juego.java
@@ -0,0 +1,65 @@
+
+package boletin101;
+
+import javax.swing.JOptionPane;
+/**
+ *En esta clase se detallara la funcionalidad del programa
+* @author sbenavidesvazquez
+* @version 12/1/15A
+ */
+
+public class Juego {
+    
+    private int n,a,r,t;
+    
+    public Juego(){
+        
+    }
+    
+    public int intentos(){
+        
+        do{
+        String dato=JOptionPane.showInputDialog("Introducir el número de intentos entre 1 y 10 incluidos");
+        n=Integer.parseInt(dato);        
+        }while(n<=0 || n>10);
+        return n;
+    }
+    
+    public int numero(){
+        
+        do{
+        String dato=JOptionPane.showInputDialog("Introducir número para adivinar");
+        a=Integer.parseInt(dato);
+        }while(a<1 || a>50);
+        return a;
+        
+    }
+    
+    public void adivinar(){
+        
+        for(t=1;t<=n && r!=a;t++){
+            
+            do{
+            String dato=JOptionPane.showInputDialog("turno "+t+" Pon tu respuesta");
+            r=Integer.parseInt(dato);
+            if(r<a){
+                JOptionPane.showMessageDialog(null, "Más alto");
+                }
+            if(r>a){
+                JOptionPane.showMessageDialog(null, "Más bajo");
+                }
+            }while(r>50 || r<0);           
+            
+        }
+    }
+    
+    public void fin(){
+        if(r==a){
+                JOptionPane.showMessageDialog(null, "Ganaste");
+            }
+            if(t>n){
+                JOptionPane.showMessageDialog(null, "Perdiste");
+            }
+    }
+    
+}
